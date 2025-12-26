1ba7f802d7609aca9ae18a8aa7315ceb6b5ef0bf
Salih Emin <914656+cerebrux@users.noreply.github.com>
2025-12-27 00:01:10 +0200

    Add support for uno_r4_wifi environment in multiple projects

--- Blink/src/main.cpp
+++ Blink/src/main.cpp
@@ -1,19 +1,5 @@
-/**
- * @file main.cpp
- * @brief LED Blink program for Arduino/PlatformIO.
- *
- * This file contains the basic code to control the built-in LED of a microcontroller,
- * as well as logging the status to the serial port.
- */
-
 #include <Arduino.h>
 
-/**
- * @brief Program initialization.
- *
- * The setup() function is called once when the microcontroller starts.
- * Here we configure serial communication and pin modes.
- */
 void setup()
 {
   // Start serial communication at 9600 baud
@@ -22,12 +8,6 @@ void setup()
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-/**
- * @brief Main execution loop.
- *
- * The loop() function runs repeatedly.
- * Here we control the LED state (ON/OFF) and print messages.
- */
 void loop()
 {
   // Turn the LED on (HIGH voltage level)
